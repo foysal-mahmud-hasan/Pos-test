c38dc99c25f77c56ae700da97c3f7b60d4549789
TatsuSolo <1730150@iub.edu.bd>
2023-03-06 19:16:53 +0600

    4th-3/6/23

--- app/src/main/java/com/wst/wst_pos/MainActivity.kt
+++ app/src/main/java/com/wst/wst_pos/MainActivity.kt
@@ -1,108 +1,27 @@
 package com.wst.wst_pos
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.SurfaceHolder
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.lifecycleScope
-import com.google.zxing.Result
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import me.dm7.barcodescanner.zxing.ZXingScannerView
+import androidx.databinding.DataBindingUtil
+import com.wst.wst_pos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding : ActivityMainBinding
 
 
 
-class MainActivity : AppCompatActivity(), SurfaceHolder.Callback, ZXingScannerView.ResultHandler {
-
-    private val CAMERA_PERMISSION_CODE = 1001
-    private lateinit var scannerView: ZXingScannerView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        scannerView = ZXingScannerView(this)
-        setContentView(scannerView)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        lifecycleScope.launch {
-            if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                startCamera()
-            } else {
-                requestCameraPermission()
-            }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val users = listOf<Users>()
+        users.
+        binding.btnLogin.setOnClickListener {
+            binding.contentLayout.visibility = View.GONE
+            binding.loadingAnimation.visibility = View.VISIBLE
         }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        scannerView.stopCamera()
-    }
-
-    override fun handleResult(rawResult: Result?) {
-        // Handle the result here, for example, you can display the text from the QR code
-        val text = rawResult?.text
-        Toast.makeText(this, "QR Code Text: $text", Toast.LENGTH_LONG).show()
-        val intent = Intent(this@MainActivity, HomeActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
-    private fun requestCameraPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-            AlertDialog.Builder(this)
-                .setTitle("Camera permission needed")
-                .setMessage("This app needs the Camera permission, please accept to use the QR Code scanner")
-                .setPositiveButton("OK") { _, _ ->
-                    // Request permission
-                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
-                }
-                .setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }
-                .create()
-                .show()
-        } else {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
-        }
-    }
-
-    private fun startCamera() {
-        scannerView.setResultHandler(this)
-        scannerView.startCamera()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            CAMERA_PERMISSION_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startCamera()
-                } else {
-                    Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    override fun surfaceCreated(holder: SurfaceHolder) {
-        // No need to implement anything here
-    }
-
-    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        // No need to implement anything here
-    }
-
-    override fun surfaceDestroyed(holder: SurfaceHolder) {
-        // No need to implement anything here
     }
 }
 
---
+++ app/src/main/java/com/wst/wst_pos/Users.kt
@@ -0,0 +1,6 @@
+package com.wst.wst_pos
+
+class Users {
+    var userName : String = ""
+    var password : String = ""
+}
